813269903c93dfe27df5f64ade1c2c41bfcdf3d8
ysera34 <ycyoon34@gmail.com>
2022-01-04 01:00:26 +0900

    kotlin concurrency chapter03 project add launch and async coroutine test

--- kotlin-concurrency/chapter03/RssReader/app/src/test/java/org/inframincer/rss/AsyncCoroutineTest.kt
+++ kotlin-concurrency/chapter03/RssReader/app/src/test/java/org/inframincer/rss/AsyncCoroutineTest.kt
@@ -3,6 +3,7 @@ package org.inframincer.rss
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 
@@ -42,7 +43,34 @@ class AsyncCoroutineTest {
             val task = GlobalScope.async {
                 doSomeThing()
             }
+            delay(2000)
+            println("Completed")
+        }
+    }
+
+    @Test
+    fun asyncTest4() {
+        runBlocking {
+            val task = GlobalScope.async {
+                doSomeThing()
+            }
             task.await()
+            println("Completed")
+        }
+    }
+
+    @Test
+    fun asyncTest5() {
+        runBlocking {
+            val task = GlobalScope.async {
+                doSomeThing()
+            }
+
+            try {
+                task.await()
+            } catch (e: Exception) {
+                println("Deferred cancelled due to ${e.message}")
+            }
             println("Completed")
         }
     }
