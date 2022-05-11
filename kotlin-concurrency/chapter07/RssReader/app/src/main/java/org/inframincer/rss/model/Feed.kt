de634fda607bd1cb9a9c85051ebb3ec9b0687461
ysera34 <ycyoon34@gmail.com>
2022-05-12 02:11:54 +0900

    kotlin concurrency chapter07 project init

---
+++ kotlin-concurrency/chapter07/RssReader/app/src/main/java/org/inframincer/rss/model/Feed.kt
@@ -0,0 +1,6 @@
+package org.inframincer.rss.model
+
+data class Feed(
+    val name: String,
+    val url: String,
+)
---
+++ kotlin-concurrency/chapter07/RssReader/app/src/test/java/org/inframincer/rss/Profile.kt
@@ -0,0 +1,7 @@
+package org.inframincer.rss
+
+data class Profile(
+    val id: Long,
+    val name: String,
+    val age: Int,
+)
